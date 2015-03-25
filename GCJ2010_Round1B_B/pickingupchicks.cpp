0ae9db41622e63dc4dae173caebd4e873820a5c8
danys <netfox2006@hotmail.com>
2015-03-26 00:00:17 +0100

    Simple solution

--- GCJ2010_Round1B_B/pickingupchicks.cpp
+++ GCJ2010_Round1B_B/pickingupchicks.cpp
@@ -7,80 +7,23 @@ using namespace std;
 
 int X[maxN];
 int V[maxN];
-vector<int> vpos[maxN];
-double totaltime[maxN];
-
-double newpos(double x0, double v, double t)
+void solve(int n,int k,int b,int t)
 {
-    return x0+v*t;
-}
-
-double ttocross(double x1,double x2,int v1,int v2)
-{
-    return (double)((double)(x2-x1)/(double)(v1-v2));
-}
-
-double ttobarn(int b, int x, int v)
-{
-    return (double)((double)(b-x)/(double)v);
-}
-
-int solve(int n,int k,int b,int t)
-{
-    double goaltime,crosstime,currentpos;
-    int currentspeed;
-    int counter;
-    counter=0;
-    for(int i=n-1;i>=0;i--)
+    int slowchicks=0;
+    int fastchicks=0;
+    int res=0;
+    for (int i=n-1;i>=0;i--)
     {
-        totaltime[i]=0;
-        goaltime = ttobarn(b,X[i],V[i]);
-        currentpos = (double)X[i];
-        currentspeed = V[i];
-        if(i<n-1)
+        if (fastchicks==k) break;
+        if (X[i]+t*V[i]<b) slowchicks++;
+        else
         {
-            vector<int> pos = vpos[i+1];
-            if (pos.size()==0)
-            {
-                crosstime = ttocross(X[i],X[i+1],V[i],V[i+1]);
-                if ((crosstime>=0) && (crosstime<goaltime))
-                {
-                    totaltime[i] += crosstime;
-                    currentpos = newpos(currentpos,currentspeed,crosstime);
-                    vpos[i].push_back(currentpos);
-                    currentspeed = V[i+1];
-                    goaltime = ttobarn(b,currentpos,currentspeed);
-                    totaltime[i] += goaltime;
-                }
-                else totaltime[i] = goaltime;
-            }
-            else
-            {
-                for(int j=0;j<pos.size();j++)
-                {
-                    //
-                }
-            }
-        }
-        else totaltime[i] = goaltime;
-        if (totaltime[i]<=t) counter++;
-    }
-    if (counter>=k) return 0;
-    else return -1;
-}
-
-bool ispossible(int n, int k, int b, int t)
-{
-    int counter=0;
-    for(int i=0;i<n;i++)
-    {
-        if (ttobarn(b,X[i],V[i])>t)
-        {
-            counter++;
-            if (counter>n-k) return false;
+            fastchicks++;
+            res += slowchicks;
         }
     }
-    return true;
+    if (fastchicks >= k) cout << res;
+    else cout << "IMPOSSIBLE";
 }
 
 int main()
@@ -92,8 +35,9 @@ int main()
         cin >> N >> K >> B >> T;
         for(int i=0;i<N;i++) cin >> X[i];
         for(int i=0;i<N;i++) cin >> V[i];
-        if (!ispossible(N, K, B, T)!=-1) cout << "Case #" << z << ": IMPOSSIBLE" << endl;
-        else  cout << "Case #" << z << ":" << solve(N,K,B,T) << endl;
+        cout << "Case #" << z << ":";
+        solve(N,K,B,T);
+        cout << endl;
     }
     return 0;
 }
